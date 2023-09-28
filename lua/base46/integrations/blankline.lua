8fb451dd3870375c78c5b7cf963882a44c678ef4
siduck <siduck@tutanota.com>
2023-09-28 11:21:31 +0530

    update indentl blanklinv v3.0 hlgroups

--- lua/base46/integrations/blankline.lua
+++ lua/base46/integrations/blankline.lua
@@ -1,8 +1,7 @@
 local colors = require("base46").get_theme_tb "base_30"
 
 return {
-  IndentBlanklineChar = { fg = colors.line },
-  IndentBlanklineSpaceChar = { fg = colors.line },
-  IndentBlanklineContextChar = { fg = colors.grey },
-  IndentBlanklineContextStart = { bg = colors.one_bg2 },
+  IblChar = { fg = colors.line },
+  IblScopeChar = { fg = colors.grey }, -- first indenline in scope only
+  IblScopeFirstLine = { bg = colors.one_bg2 },
 }
