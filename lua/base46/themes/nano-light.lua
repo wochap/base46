5dd42c657bc5bd7117979f7cb72b5cf6513157cd
tom speak <tomspeak@users.noreply.github.com>
2023-11-01 09:58:06 +0000

    upgrade nano-light (#257)
    * upgrade nord-light
    
    * Update nano-light.lua
    
    ---------
    
    Co-authored-by: Sidhanth Rathod <siduck@tutanota.com>

--- lua/base46/themes/nano-light.lua
+++ lua/base46/themes/nano-light.lua
@@ -1,9 +1,11 @@
 -- Credits to original theme https://github.com/rougier/nano-theme
 -- This is a modified version of it
 
+---@type Base46Table
 local M = {}
 
 M.base_30 = {
+  deep_black = "#263238",
   white = "#37474F",
   darker_black = "#f7f7f7",
   black = "#FFFfff", --  nvim bg
@@ -14,8 +16,8 @@ M.base_30 = {
   grey = "#c4c4c4",
   grey_fg = "#b5b5b5",
   grey_fg2 = "#a3a3a3",
-  light_grey = "#919191",
-  light_grey2 = "#848484",
+  light_grey = "#848484",
+  faded_grey = "#8497a0",
   red = "#EF5350",
   tintred = "#BF616A",
   baby_pink = "#b55dc4",
@@ -38,7 +40,6 @@ M.base_30 = {
   lightbg = "#e0e0e0",
   pmenu_bg = "#673AB7",
   folder_bg = "#4C566A",
-  tintblack = "#4C566A",
 }
 
 M.base_16 = {
@@ -48,25 +49,28 @@ M.base_16 = {
   base03 = M.base_30.grey,
   base04 = M.base_30.grey_fg,
   base05 = M.base_30.white,
-  base06 = M.base_30.tintblack,
-  base07 = "#242832",
-  base08 = M.base_30.nord_blue,
-  base09 = M.base_30.tintred,
-  base0A = M.base_30.cream,
-  base0B = M.base_30.green,
-  base0C = M.base_30.teal,
-  base0D = M.base_30.nord_blue,
-  base0E = M.base_30.purple,
-  base0F = M.base_30.light_grey,
+  base06 = M.base_30.folder_bg,
+  base07 = M.base_30.deep_black,
+  base08 = M.base_30.purple,
+  base09 = M.base_30.faded_grey,
+  base0A = M.base_30.purple,
+  base0B = M.base_30.faded_grey,
+  base0C = M.base_30.purple,
+  base0D = M.base_30.deep_black,
+  base0E = M.base_30.white,
+  base0F = M.base_30.faded_grey,
 }
 
 M.polish_hl = {
   treesitter = {
-    ["@tag.attribute"] = { fg = M.base_30.cream },
-    ["@tag"] = { fg = M.base_30.purple },
-    ["@text.uri"] = { fg = M.base_30.green },
-    ["@constant.lua"] = { fg = M.base_30.white },
-    ["@function.call"] = { fg = M.base_30.white, bold = true },
+    ["@function"] = { bold = true },
+    ["@function.builtin"] = { bold = true },
+    ["@function.call"] = { bold = true },
+    ["@constructor"] = { fg = M.base_30.purple },
+    ["@parameter"] = { fg = M.base_30.white },
+    ["@namespace"] = { fg = M.base_30.deep_black },
+    ["@symbol"] = { fg = M.base_30.purple },
+    ["@keyword"] = { fg = M.base_30.purple },
   },
 
   telescope = {
